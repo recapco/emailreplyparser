74f174e3d1d81de3e96c3f621ad804ffdd418758
Will Johnson <wjohnson191@gmail.com>
2015-07-23 12:39:50 -0700

    Removing separate file for reverse.

--- email.go
+++ email.go
@@ -158,3 +158,13 @@ func (f *Fragment) finish() {
 func (f *Fragment) String() string {
 	return f.content
 }
+
+func reverse(input string) string { // note: this will not work well with combining characters
+	reversed := []rune(input)
+
+	for i, j := 0, len(reversed)-1; i < len(reversed)/2; i, j = i+1, j-1 {
+		reversed[i], reversed[j] = reversed[j], reversed[i]
+	}
+
+	return string(reversed)
+}
